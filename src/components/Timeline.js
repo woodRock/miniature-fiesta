efd2a1edd3cbab80d28011d9803dfa49f57407c8
woodrock <j.r.h.wood98@gmail.com>
2021-09-27 13:18:40 +1300

    Fix right alignment (see: https://bit.ly/3o9bfZG)

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -10,48 +10,62 @@ import EducationIcon from "../assets/education.png";
 import WorkIcon from "../assets/work.png";
 import Home from "../assets/home.png";
 
-const VTimeline = ({markers}) => {
-  return (
-    <VerticalTimeline className={ "timeline" } >
-      <HomeButton />
-      { markers.map( ({name, url, type, date}, index) => (
-        <VerticalTimelineElement
-          className={ type == "work" ? "vertical-timeline-element--work" : "vertical-timeline-element--education" }
-          date={ date }
-          icon={ type == "work" ? <Icon source={ WorkIcon } /> : <Icon source={ EducationIcon } /> }
-          iconStyle={ { background: colors[type], color: "#fff" } }
-          key={ index }
-        >
-          <h3 className="vertical-timeline-element-title">{name}</h3>
-          <h4 className="vertical-timeline-element-subtitle">{type}</h4>
-          <p>
-            <a href={ url }> See More...</a> 
-          </p>
-        </VerticalTimelineElement>
+/**
+ * A vertical timeline from the interwebs (source: https://bit.ly/3AJfrmk).
+ * @param {markers} param0 a collection of locations with information.
+ */
+const VTimeline = ({markers}) => (
+  <VerticalTimeline className={ "timeline" } >
+    <HomeButton />
+    { markers.map( ({name, url, type, date}, index) => (
+      <VerticalTimelineElement
+        className={ type == "work" ? "vertical-timeline-element--work" : "vertical-timeline-element--education" }
+        date={ date }
+        icon={ type == "work" ? <Icon source={ WorkIcon } /> : <Icon source={ EducationIcon } /> }
+        iconStyle={ { background: colors[type], color: "#fff" } }
+        key={ index }
+      >
+        <h3 className="vertical-timeline-element-title">{name}</h3>
+        <h4 className="vertical-timeline-element-subtitle">{type}</h4>
+        <p>
+          <a href={ url }> See More...</a> 
+        </p>
+      </VerticalTimelineElement>
     ))}
-      <HomeButton />
-    </VerticalTimeline>
-  );
-    };
+    <HomeButton />
+  </VerticalTimeline>
+);
 
 VTimeline.propTypes = {
   markers: propTypes.array.isRequired
 };
 
+/**
+ * Icon for the Home button, returns user home.
+ * @returns {React.Component} a button to return to the home page.
+ */
 const HomeButton = () => {
+  // Get the history object from the react-router-dom library.
   const history = useHistory();
 
+  // Return to home page.
   function home() {
     history.push("/");
   }
+
+  // Green button with home icon.
   return (
     <VerticalTimelineElement
       icon={ <Icon source={ Home } /> }
       iconOnClick={ () => home() }
       iconStyle={ { background: "rgb(16, 204, 82)", color: "#fff" } }
     />);
-  };
+};
 
+/**
+ * An icon displayed at the center of the timeline. 
+ * @param {source} param0 an image source that has been imported. 
+ */
 const Icon = ({source}) =>  (
   <img
     src={ source } 
--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -2,6 +2,9 @@ import React from "react";
 import { markers } from "./Map";
 import VTimeline from "../components/Timeline.js";
 
+/**
+ * Displays markers from map, instead as a timeline.
+ */
 const Timeline = () => (
   <VTimeline markers={ markers } />
 );
